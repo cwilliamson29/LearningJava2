6df2bacc8d7f6aadd99456c07aa78286f18bede8
Chris Williamson <chriswill0629@gmail.com>
2024-05-24 10:24:03 -0400

    Lesson 8 comparing objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var textBox = new TextBox();
+       // var textBox = new TextBox();
+
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1,2);
+        System.out.println(point1 == point2);
+        System.out.println(point1.equals(point2));
     }
 }
